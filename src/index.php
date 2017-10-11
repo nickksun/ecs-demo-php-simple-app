edcda486dffc0d4efba33e08116d549c74eff4df
Sun <yaoys@784f43a2ea2b.ant.amazon.com>
2017-10-11 14:02:24 +1100

    Commit to 2

--- src/index.php
+++ src/index.php
@@ -15,14 +15,14 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App 1</h1>
+                <h1>Simple PHP App 2</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
+                    $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+                    echo fread($myfile,filesize("/var/www/my-vol/date"));
+                    fclose($myfile);
                 ?>
 
             </div>
